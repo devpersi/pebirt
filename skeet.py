b095d9a38acbbd824d5b57e559d098ef1ceaad0c
devpersi <109619062+devpersi@users.noreply.github.com>
2025-06-19 00:14:12 +0300

    more code golfing

--- skeet.py
+++ skeet.py
@@ -1,8 +1,5 @@
 # this version can fit in a bluesky post
-from PIL import Image
-from pathlib import Path
-
-for f in Path('').glob('*.png'):
-    with Image.open(f) as img:
-        s=min(img.size)
-        img.crop(((img.width-s)//2,(img.height-s)//2,(img.width+s)//2,(img.height+s)//2)).resize((800, 800), Image.LANCZOS).save(f.with_suffix('.out.png'))
+from PIL import Image; from pathlib import Path
+for f in Path().glob('*.png'):
+    with Image.open(f) as i:
+        i.crop(((w:=i.width-(s:=min(i.size)))//2, (h:=i.height-s)//2, w//2+s, h//2+s)).resize((800,800), Image.LANCZOS).save(f.with_suffix('.out.png'))
